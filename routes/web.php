5d9b716f745470dbe8358356c631a8e67c09708c
Mehedi Hassan <mh93075@gmail.com>
2024-04-24 23:36:47 +0600

    front end home section done

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.layouts.template');
 });
 
 Route::get('/admin/dashboard', function () {
